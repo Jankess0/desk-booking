7e344448628b699fe7b60cc2790fe20080440ac7
Jankess0 <kamilojanik@gmail.com>
2026-05-25 20:12:00 +0200

    routing config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login'; 
+import { UserPanel } from './user-panel/user-panel'; 
+import { AdminPanel } from './admin-panel/admin-panel';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: 'user', component: UserPanel },
+  { path: 'admin', component: AdminPanel },
+  { path: '', redirectTo: '/login', pathMatch: 'full' } 
+];
