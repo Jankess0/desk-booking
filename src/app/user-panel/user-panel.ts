b8910365f97141595e4da19580d0be469057cdbe
Jankess0 <kamilojanik@gmail.com>
2026-05-25 20:02:15 +0200

    generated componets: login, user-panel, admin-panel and auth service

---
+++ src/app/user-panel/user-panel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-panel',
+  imports: [],
+  templateUrl: './user-panel.html',
+  styleUrl: './user-panel.css',
+})
+export class UserPanel {}
